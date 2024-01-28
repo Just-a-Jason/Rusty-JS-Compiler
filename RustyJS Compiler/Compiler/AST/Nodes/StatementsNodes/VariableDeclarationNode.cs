8cd4a92dac80017f1e9699d1790a97b5e80b2220
Jason.json <88512392+Just-a-Jason@users.noreply.github.com>
2024-01-28 23:14:57 +0100

    VariableDeclarationNode

---
+++ RustyJS Compiler/Compiler/AST/Nodes/StatementsNodes/VariableDeclarationNode.cs
@@ -0,0 +1,11 @@
+﻿class VariableDeclarationNode : StatementNode {
+    public override NodeType Kind { get; } = NodeType.VariableDeclaration;
+    public RuntimeValueTypeNode ValueType { get; }
+    public RuntimeValueTypeNode Value { get; }
+    public bool IsConstant { get; }
+    public bool IsStatic { get; }
+    public bool IsMutable { get; }
+    public string Name { get; }
+
+}
+
