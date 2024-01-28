ac3c70b6a2d91311fca2cc4799b4b3aae26fc9f6
Jason.json <88512392+Just-a-Jason@users.noreply.github.com>
2024-01-28 22:08:23 +0100

    scoping lol

---
+++ RustyJS Compiler/Compiler/SubModules/RustyEnviroment.cs
@@ -0,0 +1,6 @@
+﻿internal class RustyEnvironment {
+    private Dictionary<string, RuntimeValueTypeNode> _varaiables  = new Dictionary<string, RuntimeValueTypeNode>();
+    private RustyEnvironment? _parent;
+
+}
+
